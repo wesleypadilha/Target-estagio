5bf384c49129fa5cd3f9b32232925fefb130328c
wesleypadilha <wtpadilhaa@gmail.com>
2024-09-04 11:35:53 -0300

    Adiciona códigos para os exercícios solicitados

---
+++ target4.py
@@ -0,0 +1,14 @@
+faturamento = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+}
+
+total = sum(faturamento.values())
+
+percentuais = {estado: (valor / total) * 100 for estado, valor in faturamento.items()}
+
+for estado, percentual in percentuais.items():
+    print(f"O percentual de representação de {estado} é: {percentual:.2f}%")
